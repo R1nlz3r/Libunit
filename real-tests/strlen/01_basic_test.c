a34106c307b0ef4956b9686b6adde296672769e8
Mathis PANDELE <mapandel@e2r2p22.42.fr>
2017-02-11 14:08:07 +0100

    Premiers tests

--- real-tests/strlen/01_basic_test.c
+++ real-tests/strlen/01_basic_test.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 11:45:18 by mapandel          #+#    #+#             */
-/*   Updated: 2017/02/11 12:09:50 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/02/11 14:06:51 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int		basic_test(void)
 {
 	char	test[20];
 
-	test = "Les roses sont roses";
+	test = ft_strfill(test, 'a', 20);
 	if (strlen(test) == ft_strlen(test))
 		return (0);
 	else
